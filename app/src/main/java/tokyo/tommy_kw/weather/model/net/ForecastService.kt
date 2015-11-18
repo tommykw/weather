be5f49fb2f09f77f2eb9e9797027849673cd45c3
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-11-18 23:37:49 +0900

    Fix return param. Changed Obervable<String> to Observable<Weather>

--- app/src/main/java/tokyo/tommy_kw/weather/model/net/ForecastService.kt
+++ app/src/main/java/tokyo/tommy_kw/weather/model/net/ForecastService.kt
@@ -4,11 +4,12 @@ import retrofit.http.GET
 import retrofit.http.POST
 import retrofit.http.Query
 import rx.Observable
+import tokyo.tommy_kw.weather.model.entity.request.Weather
 
 /**
  * Created by tommy on 15/11/18.
  */
 interface ForecastService {
     @GET("/")
-    operator fun get(@Query("lat") lat: Float, @Query("dat") dat: Float): Observable<String>
+    operator fun get(@Query("lat") lat: Float, @Query("dat") dat: Float): Observable<Weather>
 }
